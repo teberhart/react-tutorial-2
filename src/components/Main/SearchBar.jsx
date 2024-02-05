65ff4ccd1c09b403288466b35446783ecd6e8afe
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 12:46:45 +0100

    tutorial 49, made fetch function async using await, and reset of input async using then

--- src/components/Main/SearchBar.jsx
+++ src/components/Main/SearchBar.jsx
@@ -6,10 +6,37 @@ import {faSearchengin} from "@fortawesome/free-brands-svg-icons";
 export default function SearchBar() {
     const [inputValue, setInput] = useState("");
 
+    async function fetchRecipes (queryString) {
+        const ENDPOINT = "https://api.spoonacular.com/recipes/complexSearch";
+        const APIKEY = import.meta.env.VITE_SPOONACULAR_API_KEY;
+        const query = `${ENDPOINT}?apiKey=${APIKEY}&query=${queryString}`;
+
+        const results = await fetch(query);
+        const data = await results.json();
+
+        // data looks like
+        /*
+        data:
+        [
+            "number": 10,
+            "offset": 0,
+            "results": [
+                0 [
+                    "id": 1234
+                    "image": "http://image.com/image.jpg"
+                    "imageType": "jpg"
+                    "title": "Cool recipe"
+                ]
+            ],
+            "totalResults": 35
+        ]
+         */
+    }
+
     // Using a button, to avoid having multiple calls burning through the API's daily request limit
     function handleSubmit(e) {
         e.preventDefault();
-        setInput("");
+        fetchRecipes(inputValue).then(r => setInput(""));
     }
 
     return (
