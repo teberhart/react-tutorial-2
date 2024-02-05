e68c5ef4756a1d545073664275b51f53df0e0693
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 12:19:22 +0100

    tutorial 48, added FA, styled the searchbar a little

--- src/components/Main/SearchBar.jsx
+++ src/components/Main/SearchBar.jsx
@@ -1,22 +1,31 @@
 import {useState} from "react";
+import styles from './SearchBar.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearchengin} from "@fortawesome/free-brands-svg-icons";
 
 export default function SearchBar() {
     const [inputValue, setInput] = useState("");
 
+    // Using a button, to avoid having multiple calls burning through the API's daily request limit
     function handleSubmit(e) {
         e.preventDefault();
         setInput("");
     }
 
     return (
-        <div>
+        <div className={styles.main}>
             <input
+                className={styles.input}
                 type="text"
                 value={inputValue}
                 placeholder="Type to search for recipes"
                 onChange={(e)=>setInput(e.target.value)}
             />
-            <button onClick={(e) => handleSubmit(e)}>Search</button>
+            <button
+                className={styles.button}
+                onClick={(e) => handleSubmit(e)}>
+                <FontAwesomeIcon icon={faSearchengin} />
+            </button>
         </div>
     );
 }
